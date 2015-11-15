b9812e8da26e08d6aeab3b180a0ac0ad6ee684b0
왕승현 <motaesolo123@gmail.com>
2015-11-15 18:09:42 +0900

    총수익률구현, usermoney데이터생성
    ㄹㅇ 끝 (아마도..)

--- common/cron.py
+++ common/cron.py
@@ -1,13 +1,14 @@
 import random
 from datetime import datetime
 
-from common.models import Stock, StockPrice, News, Newslist, Sospi
+from common.models import Stock, StockPrice, News, Newslist, Sospi, HaveStock, UserProfile
 
 
 def add_news():
     news = News.objects.all()
     stocks = Stock.objects.all()
     stockp = StockPrice.objects.all()
+    user = UserProfile.objects.all()
     sospi = Sospi.objects
     today = datetime.now()
     var_list = []
@@ -26,8 +27,30 @@ def add_news():
         createnews = Newslist.objects.create(content=content, stock=stocks[i].StockItem)
         createnews.save()
 
-    average = sum(var_list) / len(stocks)
-    nowsospi = sospi.all().order_by('-id')[0].data
+        average = sum(var_list) / len(stocks)
+        nowsospi = sospi.all().order_by('-id')[0].data
+        s = Sospi.objects.create(data=(average * 10 + nowsospi), day=int(today.day), month=int(today.month), fluctuation=average)
+        s.save()
 
-    s = Sospi.objects.create(data=(average * 10 + nowsospi), day=int(today.day), month=int(today.month), fluctuation=average)
-    s.save()
+        for i in user:
+            have_stock = HaveStock.objects.filter(owner=i)
+            money = i.usermoney
+            for j in have_stock:
+                id = StockPrice.objects.all().filter(id=j.my_stock_id)[0].StockItem_id
+                nowprice = StockPrice.objects.all().filter(StockItem_id=id).order_by('-id')[0].StockPrice
+                money += nowprice * j.count
+            UserProfile.objects.filter(id=i.id).update(usermoney=money)
+
+def compare():
+    user = UserProfile.objects.all()
+    for i in user:
+        havestock = HaveStock.objects.filter(owner=i.id)
+        money = i.usermoney
+        for j in havestock:
+            id = StockPrice.objects.all().filter(id=j.my_stock_id)[0].StockItem_id
+            nowprice = StockPrice.objects.all().filter(StockItem_id=id).order_by('-id')[0].StockPrice
+            money += nowprice * j.count
+        UserProfile.objects.filter(id=i.id).update(old_usermoney=money)
+
+
+
