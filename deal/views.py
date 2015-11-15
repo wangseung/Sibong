ca7de5352c0d97dd4e9d8e93e5e9135892496673
L3opold7 <l3opold7@gmail.com>
2015-11-15 23:34:04 +0900

    마지막 수정이 되기를 바란다 나의 커밋이여!

--- deal/views.py
+++ deal/views.py
@@ -35,7 +35,7 @@ def stock_request(request):
                 return render_to_response('after_deal.html', context=RequestContext(request,{'price': stock.StockPrice*count,}))
             if stock_check.count() > 0:
                 count += stock_check[0].count
-                have = HaveStock.objects.get(owner=usecdr, my_stock=stock)
+                have = HaveStock.objects.get(owner=user, my_stock=stock)
                 have.count = count
                 user.usermoney -= stock_check[0].count * stock.StockPrice
                 user.save()
@@ -51,16 +51,27 @@ def stock_request(request):
             stocklist = StockPrice.objects.filter(StockItem=stockitem)
             stock = stocklist[len(stocklist)-1]
             user = UserProfile.objects.get(username=request.user.username)
-            stock_check = HaveStock.objects.filter(owner=user, my_stock=stock)
+            stock_check = list()
+            dic = {'data':1}
+            havelist = HaveStock.objects.filter(owner=user)
+            for i in havelist:
+                if i.my_stock.StockItem == stockitem:
+                    stock_check.append(i)
+            for i in stock_check:
+                if count <= i.count:
+                    user.usermoney += stock.StockPrice * count
+                    i.count -= count
+                    if i.count <= 0:
+                        i.delete()
+                    else:
+                        i.save()
+                    user.save()
+                else:
+                    return render_to_response('after_deal.html',
+                                              context=RequestContext(request,{'data': 1,'content':"개수가 부족합니다!"}))
 
-            if stock_check.count() > 0 and count <= stock_check[0].count:
-                have = HaveStock.objects.get(owner=user, my_stock=stock)
-                user.usermoney += stock.StockPrice * count
-                have.count -= count
-                have.save()
-                user.save()
-            else:
-                return render_to_response('after_deal.html', context=RequestContext(request,{'data':'True'}))
+            return render_to_response('after_deal.html',
+                                      context=RequestContext(request,{'data': 1,'content':"개수가 부족합니다!"}))
         else:
             raise Http404()
 
--- mypage/views.py
+++ mypage/views.py
@@ -8,20 +8,6 @@ import operator
 def get_rank(request):
     users = UserProfile.objects.all().order_by('-usermoney')
     users_money = list()
-    """
-    dic = {}
-    for i in users:
-        have_stock = HaveStock.objects.filter(owner=i)
-        money = i.usermoney
-        for j in have_stock:
-            stockprice_list = StockPrice.objects.filter(StockItem=j.my_stock.StockItem)
-            stockprice = stockprice_list[len(stockprice_list)-1]
-            money += stockprice.StockPrice * j.count
-        dic[i.username]=money
-    sorted_dic = sorted(dic.items(), key=operator.itemgetter(1), reverse=True)
-    for i in sorted_dic:
-        users_money.append(dict(name=str(i[0]), asset=int(i[1])))
-    """
     for i in users:
         users_money.append(dict(name=str(i.username), asset=int(i.usermoney)))
     send_userlist = str(users_money).replace(chr(39),chr(34))
@@ -37,7 +23,7 @@ def get_account(request):
         stocklist = StockPrice.objects.filter(StockItem=i.my_stock.StockItem)
         now_price = stocklist[len(stocklist)-1].StockPrice
         if i.count <= 0:
-            pass
+            i.delete()
         else:
             sendlist.append(dict(item=str(i.my_stock.StockItem.StockItem), past=i.buy_price,
                                 profit=int(now_price - i.buy_price),
@@ -51,12 +37,6 @@ def get_earn(request):
     user = UserProfile.objects.get(username=request.user.username)
     have_stock = HaveStock.objects.filter(owner=user)
     money = user.usermoney
-    """
-    for i in have_stock:
-        stockprice_list = StockPrice.objects.filter(StockItem=i.my_stock.StockItem)
-        stockprice = stockprice_list[len(stockprice_list)-1]
-        money += stockprice.StockPrice * i.count
-    """
     earn = (user.usermoney - user.old_usermoney) / user.old_usermoney * 100
     if earn < 0:
         earn *= -1
@@ -75,12 +55,6 @@ def get_value(request):
     user = UserProfile.objects.get(username=request.user.username)
     have_stock = HaveStock.objects.filter(owner=user)
     money = user.usermoney
-    """
-    for i in have_stock:
-        stockprice_list = StockPrice.objects.filter(StockItem=i.my_stock.StockItem)
-        stockprice = stockprice_list[len(stockprice_list)-1]
-        money += stockprice.StockPrice * i.count
-    """
     send_value.append(dict(value=money))
     send_value = str(send_value).replace(chr(39),chr(34))
     return HttpResponse(send_value, content_type='application/json')
